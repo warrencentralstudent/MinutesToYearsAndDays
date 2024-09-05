bf90057f63b95fa1b861eb221223ea7957bad5a8
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-05 11:53:59 -0400

    yup

--- src/MinutesToYearsAndDays.java
+++ src/MinutesToYearsAndDays.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MinutesToYearsAndDays {
+class Main {
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+
+    }
+
+    private static void printYearsAndDays(long minutes) {
+        long hours = minutes / 60;
+        long days = hours / 24;
+        long years = days / 365;
+        long leftoverDays = years % 365;
+    }
+
+
 }
